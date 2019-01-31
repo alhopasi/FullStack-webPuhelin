22073eb113987cea25690364115e190d8c9ae8d4
Pasi Alho <alhopasi@cs.helsinki.fi>
2019-01-31 14:19:20 +0200

    osa3c

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -17,46 +19,46 @@ const morganLog = morgan(':method :url :status :res[content-length] - :response-
 
 app.use(morganLog)
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '045-1236543',
-  },
-  {
-    id: 2,
-    name: 'Arto Järvinen',
-    number: '041-21423123',
-  },
-  {
-    id: 3,
-    name: 'Lea Kutvonen',
-    number: '040-4323234',
-  },
-  {
-    id: 4,
-    name: 'Martti Tienari',
-    number: '09-784232',
-  },
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
+
+  })
+  .catch(error => next(error))
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(204).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -64,51 +66,45 @@ app.get('/info', (request, response) => {
   const reply = ""
   response.send(`<p>Puhelinluettelossa ${amount} henkilön tiedot</p>
   <p>${new Date()}</p>`)
-
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  
   const body = request.body
-
-  if (body.name === undefined) {
-    return response.status(400).json({
-      error: 'name missing'
-    })
-  }
-
-  if (body.number === undefined) {
-    return response.status(400).json({
-      error: 'number missing'
-    })
-  }
-
-  persons.map(n => {
-    if (body.name === n.name) {
-      return response.status(400).json({
-        error: 'name must be unique'
-      })
-    }
-  })
-
+  console.log('BODY:', body)
   const person = {
-    id: Math.floor(Math.random() * 10000),
     name: body.name,
-    number: body.number
+    number: body.number,
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson.toJSON())
+  })
+  .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`)
+}
+)
+
