d602c61a88e4cd2350a37717d7fc9250919a684e
Pasi Alho <alhopasi@cs.helsinki.fi>
2019-01-31 18:11:20 +0200

    osa3.21 uusiksi

---
+++ build/precache-manifest.ed5aa280183478c29a28801e2d8c3a01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39712a927fe98e738c7",
+    "url": "/static/css/main.12d5d299.chunk.css"
+  },
+  {
+    "revision": "a39712a927fe98e738c7",
+    "url": "/static/js/main.a39712a9.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0220ee10015158ff2fba62b10a951ab3",
+    "url": "/index.html"
+  }
+];
