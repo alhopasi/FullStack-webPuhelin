b0d4a6232d0421a3f34181381e52c02ee8e7bc1a
Pasi Alho <alhopasi@cs.helsinki.fi>
2019-01-31 21:44:41 +0200

    osa3 done

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 
-morgan.token('data', function (req, res) {
-  return JSON.stringify(req.body)
+morgan.token('data', function (request) {
+  return JSON.stringify(request.body)
 })
 
 const morganLog = morgan(':method :url :status :res[content-length] - :response-time ms :data',
@@ -32,7 +32,7 @@ app.get('/api/persons', (request, response) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (body.name === undefined || body.number === undefined) {
+  if (body.name === undefined || body.number === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
 
@@ -44,7 +44,7 @@ app.post('/api/persons', (request, response, next) => {
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON())
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 
@@ -61,38 +61,37 @@ app.get('/api/persons/:id', (request, response, next) => {
 })
 
 app.get('/info', (request, response) => {
-  const amount = persons.length
-  const reply = ""
-  response.send(`<p>Puhelinluettelossa ${amount} henkilön tiedot</p>
-  <p>${new Date()}</p>`)
+  Person.find({})
+    .then(persons => {
+      response.send(`<p>Puhelinluettelossa ${persons.length} henkilön tiedot</p>
+      <p>${new Date()}</p>`)
+    })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
+    .then(response.status(204).end())
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  
+
   const body = request.body
   const person = {
     name: body.name,
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
-  .then(updatedPerson => {
-    response.json(updatedPerson.toJSON())
-  })
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
